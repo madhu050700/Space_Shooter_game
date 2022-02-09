cdf8161b526229b74fe3a6a49f01da168220a338
Ahsun Tariq <ahsun.tariq@hotmail.com>
2022-02-09 14:10:52 -0800

    at - create scrolling background

--- desktop/src/com/l2p/game/DesktopLauncher.java
+++ desktop/src/com/l2p/game/DesktopLauncher.java
@@ -9,6 +9,10 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		int height =1024;
+		int width = 600;
+		config.setWindowSizeLimits(width,height,width,height);
+
 		new Lwjgl3Application(new SpaceShooter(), config);
 	}
 }
