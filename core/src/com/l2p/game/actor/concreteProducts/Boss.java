4edb0398be2f9b662b273cfc04fe6093c751c394
Ahsun Tariq <ahsun.tariq@hotmail.com>
2022-03-27 19:13:56 -0700

    switch boss movement on 50% health

--- core/src/com/l2p/game/actor/concreteProducts/Boss.java
+++ core/src/com/l2p/game/actor/concreteProducts/Boss.java
@@ -6,12 +6,15 @@ import com.badlogic.gdx.math.Vector2;
 import com.l2p.game.SpaceShooter;
 import com.l2p.game.actor.abstractProducts.Actor;
 import com.l2p.game.movement.abstractProducts.Movement;
+import com.l2p.game.movement.factories.MovementFactoryBuilder;
 import com.l2p.game.projectile.concreteProducts.MidBossProjectile;
 import com.l2p.game.projectile.abstractProducts.Projectile;
 import com.l2p.game.projectile.factories.MidBossProjectileFactory;
 import com.l2p.game.projectile.factories.ProjectileFactory;
 
 import java.util.LinkedList;
+
+
 
 public class Boss extends Actor {
     float projectile_x1,projectile_x2, projectile_y;
@@ -22,13 +25,15 @@ public class Boss extends Actor {
     float sizeMultiplier = 2;
     Movement bossMovement;
 
+    int startingHealth;
+
     public Boss(float movementSpeed, int health, float width, float height, float center_x, float center_y, float timeBetweenShots, float projectileWidth, float projectileHeight, float projectileSpeed, Texture actorTexture, Texture projectileTexture,
                 float projectile_x1, float projectile_x2, float projectile_y, String movementType  ) {
         super(movementSpeed, health, width, height, center_x, center_y, timeBetweenShots, projectileWidth, projectileHeight, projectileSpeed, actorTexture, projectileTexture, movementType);
 
 
         directionVector1 = new Vector2(0,-1);
-
+        this.startingHealth = health;
         this.projectile_x1 = projectile_x1;
         this.projectile_x2 =projectile_x2;
         this.projectile_y = projectile_y;
@@ -87,6 +92,24 @@ public class Boss extends Actor {
     @Override
     public void draw(Batch batch) {
         batch.draw(actorTexture, boundingBox.x, boundingBox.y, boundingBox.width, boundingBox.height);
+    }
+
+    @Override
+    public int hit(Projectile projectile){
+        if(health > 0) {
+            health --;
+
+            if(health <= startingHealth/2)
+            {
+                System.out.println("switching movement now");
+                this.movementSpeed += 2;
+                movementFactory = MovementFactoryBuilder.getFactory("circular");
+                bossMovement = movementFactory.createMovement(this.boundingBox);
+            }
+        }
+        else
+            health=0;
+        return health;
     }
 
 }
